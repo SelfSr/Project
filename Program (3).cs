09f7fb50b29394c3ce54aa7c41bbbcbcc9d39df8
SelfSr <alexey_belochkin@mail.ru>
2023-04-12 20:25:13 +0300

    Create Program (3).cs

---
+++ Program (3).cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string zodiacSing;
+            string workPlace;
+            string age;
+
+            Console.Write("Ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Сколько вам лет: ");
+            age = Console.ReadLine();
+            Console.Write("Какой ваш знак зодиака: ");
+            zodiacSing = Console.ReadLine();
+            Console.Write("Ваше место работы: ");
+            workPlace = Console.ReadLine();
+
+            Console.WriteLine($"\nВас зовут {name}, вам {age}, вы {zodiacSing} и ваше место работы: {workPlace}");
+        }
+    }
+}
