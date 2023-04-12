de21c4769190e9b48253ff2d51f1a31ef3f76486
SelfSr <alexey_belochkin@mail.ru>
2023-04-12 18:53:05 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ЯЮниор");
+        }
+    }
+}
