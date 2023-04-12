cd164881c209cdba81b580ed4fd4dfffdb44b0f9
SelfSr <alexey_belochkin@mail.ru>
2023-04-12 19:21:24 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace _14
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ЯЮниор");
+            byte healthHero = 100;
+            sbyte armorEnemy = 100;
+            short temp = 50;
+            ushort amountGold = 1000;
+            int sleepTime = 2000;
+            uint yearCount = 2;
+            long distance = 223333;
+            ulong playerId = 00000323100;
+            float jumpForce = 3.2f;
+            double carSpeed = 93.3;
+            char symbol = '@';
+            string nameHero = "Self";
+            bool isInfected = false;
         }
     }
 }
