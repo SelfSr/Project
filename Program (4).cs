85afdaf0f52aefac785aac389dc6c20232714aed
SelfSr <alexey_belochkin@mail.ru>
2023-04-12 21:02:44 +0300

    Create Program (4).cs

---
+++ Program (4).cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Белочкин";
+            string lastName = "Алексей";
+            string tempString;
+
+            Console.WriteLine($"Имя: {firstName}, Фамилия: {lastName}");
+
+            tempString = firstName;
+            firstName = lastName;
+            lastName = tempString;
+
+            Console.WriteLine($"Имя: {firstName}, Фамилия: {lastName}");
+        }
+    }
+}
