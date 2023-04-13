cdf312558fa39c0fc8a4a162c6588086156248e3
SelfSr <130439081+SelfSr@users.noreply.github.com>
2023-04-13 14:23:13 +0300

    Update Program (5).cs

--- Program (5).cs
+++ Program (5).cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-
+using System;
 namespace _14
 {
     internal class Program
@@ -8,11 +6,10 @@ namespace _14
         static void Main(string[] args)
         {
             int totalPictures = 52;
-            int PicturesInRow = 3;
-
-            int filledRows = totalPictures / PicturesInRow;
-            int leftPictures = totalPictures % PicturesInRow;
-
+            int picturesInRow = 3;
+            
+            int filledRows = totalPictures / picturesInRow;
+            int leftPictures = totalPictures % picturesInRow;
             Console.WriteLine($"Количество полностью заполненых рядов - {filledRows}, оставшиеся картинки - {leftPictures}");
         }
     }
