ec7b656c0a820c53dbfa22d8d2557b941a576e5c
SelfSr <alexey_belochkin@mail.ru>
2023-04-13 12:17:43 +0300

    Create Program (5).cs

---
+++ Program (5).cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalPictures = 52;
+            int PicturesInRow = 3;
+
+            int numberOfRows = totalPictures / PicturesInRow;
+            int leftPictures = totalPictures % PicturesInRow;
+
+            Console.WriteLine($"Количество полностью заполненых рядов - {numberOfRows}, оставшиеся картинки - {leftPictures}");
+        }
+    }
+}
