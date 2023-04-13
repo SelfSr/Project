8e0687f3eec90469538118cb88b5f0ff895c247b
SelfSr <alexey_belochkin@mail.ru>
2023-04-13 13:47:30 +0300

    Create Program (7).cs

---
+++ Program (7).cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountPeople;
+            int timeWaitMinutes;
+            int timeWaitHour;
+            int timeForPersonInMinutes = 10;
+            int minutesInHour = 60;
+
+            Console.Write("Введите количество людей перед вами: ");
+            amountPeople = int.Parse(Console.ReadLine());
+
+            timeWaitMinutes = amountPeople * timeForPersonInMinutes % minutesInHour;
+            timeWaitHour = amountPeople * timeForPersonInMinutes / minutesInHour;
+
+            if (timeWaitHour == 0)
+                Console.WriteLine($"Вы должны отстоять в очереди {timeWaitMinutes} м..");
+            else if (timeWaitMinutes == 0)
+                Console.WriteLine($"Вы должны отстоять в очереди {timeWaitHour} ч.");
+            else
+                Console.WriteLine($"Вы должны отстоять в очереди {timeWaitHour} ч. и {timeWaitMinutes} м.");
+        }
+    }
+}
