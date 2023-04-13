305ee01996ab4ed5cdace82e2e14d9d6781c6d51
SelfSr <alexey_belochkin@mail.ru>
2023-04-13 13:00:12 +0300

    Create Program (6).cs

---
+++ Program (6).cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int amountGold;
+            int crystalsToBuy;
+            int priseCrystal = 15;
+
+            Console.Write($"Добро пожаловать в лавку!\nCколько у вас золота: ");
+            amountGold = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nЦена за один кристалл - {priseCrystal} золота.\nСколько хотите купить кристаллов?");
+            crystalsToBuy = int.Parse(Console.ReadLine());
+
+            amountGold -= crystalsToBuy * priseCrystal;
+
+            Console.WriteLine($"\nВы купили {crystalsToBuy} кристаллов, у вас осталось {amountGold} золота.");
+        }
+    }
+}
