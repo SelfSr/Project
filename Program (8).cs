cecdd1486327ff0124c020bcc8c81f42c71cb673
SelfSr <alexey_belochkin@mail.ru>
2023-04-25 14:20:13 +0300

    Create Program (8).cs

---
+++ Program (8).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Projecks
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество rub");
+            double a = double.Parse(Console.ReadLine());
+
+            double converts = a * 0.015982;
+            double converts1 = a * 0.59022;
+            Console.WriteLine("Вы получите " + Math.Round(converts, 2) + "$");
+            Console.WriteLine("Вы получите " + Math.Round(converts1, 2) + " ua");
+
+
+        }
+
+    }
+}
